041316d25f7ce0efe530d7f81b047f4a7ebb4415
Eric Nichols <epnichols@gmail.com>
2015-10-24 16:09:12 +0200

    Refactoring window_size, finishing basic compose functionality

--- compose.py
+++ compose.py
@@ -14,38 +14,42 @@ SAMPLE_DATASET = np.matrix([[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0
                             [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,1,0],   # C / CMaj
                             [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,1],   # C / CMaj
                             [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,1,1,0,0,1,0,0]])  # D / GMaj
+SEED = SAMPLE_DATASET
 
 class Composer:
     """Base Class for our deep composer.
     """
 
+    window_size = None
     dataset = None  # each column is one "frame" of data
     model = None
 
-    def __init__(self):
-        self.dataset = _get_dataset()
+    def __init__(self, window_size=DEFAULT_WINDOW_SIZE):
+        self.dataset = self._get_dataset()
+        self.window_size = window_size
 
 
-    def _compile_model(self, window_size):
+    def _compile_model(self):
         # build the model: 2 stacked LSTMs
         print('Building the composer net...')
         self.model = Sequential()
-        self.model.add(LSTM(50, return_sequences=True, input_shape=(window_size, self.dataset.shape[1])))
+        self.model.add(LSTM(50, return_sequences=True, input_shape=(self.window_size, self.dataset.shape[1])))
         self.model.add(Dropout(0.2))
         self.model.add(LSTM(50, return_sequences=False))
         self.model.add(Dropout(0.2))
         self.model.add(Dense(self.dataset.shape[1]))
-        self.model.add(Activation('relu'))  # Rectified Linear Unit
+        #self.model.add(Activation('relu'))  # Rectified Linear Unit # TODO: 'relu' missing in my theano install
+        self.model.add(Activation('sigmoid'))  # Sigmoid
 
         self.model.compile(loss='mean_squared_error', optimizer='rmsprop')
 
 
-    def train(self, n_epoch=1, window_size=DEFAULT_WINDOW_SIZE):
+    def train(self, n_epoch=1):
         """Train the neural net
         """
 
         # Chop up dataset into examples with window-size N
-        training_examples = self._get_data_subsets(window_size=window_size)
+        training_examples = self._get_data_subsets()
         print('# training sequences:', len(training_examples))
 
         # Split into inputs and outputs
@@ -53,7 +57,7 @@ class Composer:
         training_examples_y = [ex[-1:] for ex in training_examples]  # outputs
 
         # Build/compile the model
-        self._compile_model(window_size)
+        self._compile_model()
 
         # Train the model
         for iteration in range(1, 60):
@@ -67,32 +71,28 @@ class Composer:
     def compose(self, num_measures=16):
         """Use a pre-trained neural network to compose a melody.
         """
-        melody_start = SAMPLE_DATASET
-        print('----- Generating with seed: "' + melody_start + '"')
+        melody = [frame for frame in SEED]
 
-        #sys.stdout.write(generated)
+        print('----- Generating with seed: "' + melody + '"')
 
-        for iteration in range(num_measures * BEATS_PER_MEASURE - len(melody_start)):
-            pass
-            # preds = model.predict(x, verbose=0)[0]
-            # next_index = sample(preds, diversity)
-            # next_char = indices_char[next_index]
-            #
-            # generated += next_char
-            # sentence = sentence[1:] + next_char
-            #
-            # sys.stdout.write(next_char)
-            # sys.stdout.flush()
+        for i in range(num_measures * BEATS_PER_MEASURE - len(melody_start)):
+            x = melody[i:i + self.window_size]
+            # TODO: need to add one more dimension with x as only element?
+
+            next_frame = self.model.predict(x, verbose=0)[0]
+            melody.append(next_frame)
+        print(melody)
         print()
 
     def _get_dataset(self):
         return SAMPLE_DATASET
 
-    def _get_data_subsets(self, window_size):
+    def _get_data_subsets(self):
         """Return N - window_size example matrices, each with window_size vectors.
         """
-        return (self.dataset[i:i+window_size] for i in range(0, len(self.dataset) - window_size))
+        return [self.dataset[i:i+self.window_size] for i in range(0, len(self.dataset) - self.window_size)]
 
 
 if __name__ == '__main__':
-    pass
+    bach = Composer()
+    bach.train()
