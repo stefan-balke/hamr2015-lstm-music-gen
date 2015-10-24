1db220ab23d4bfa01982f2b79b448643a3746d18
Ryan Groves <groves.ryan@gmail.com>
2015-10-24 18:36:14 +0200

    Made some fixes on duration and quantization

--- importer/rolling_stone.py
+++ importer/rolling_stone.py
@@ -20,7 +20,7 @@ class ImporterRollingStone(ImporterBase):
         path_songs = os.path.join(self.path, '*.nlt')
 
         for cur_path_song in glob.glob(path_songs):
-            cur_song_events = []
+            cur_melody_events = []
 
             # prevent from trying to read empty files
             if os.stat(cur_path_song).st_size == 0:
@@ -35,9 +35,9 @@ class ImporterRollingStone(ImporterBase):
                     # every event consists of
                     # Onset, Position in bar, Pitch, Scale Degree
                     cur_event_list = [float(x) for x in cur_event.rstrip().split("\t")]
-                    cur_song_events.append(cur_event_list)
+                    cur_melody_events.append(cur_event_list)
 
-            self.import_piano_roll(np.asarray(cur_song_events))
+            self.import_piano_roll(np.asarray(cur_melody_events))
 
     def import_piano_roll(self, note_events):
         # get the number of total bars
@@ -50,12 +50,12 @@ class ImporterRollingStone(ImporterBase):
         # pitch_range_end = np.max(note_events[:, 2])
         # print(pitch_range_start, pitch_range_end, pitch_range_end-pitch_range_start)
 
-        # set the note length as the derivative of the onsets
+        # set the note length as the interval between consecutive metric onsets
         # (suggested in the documentation)
-        note_events = np.c_[note_events[:-1], np.diff(note_events[:, 0])]
+        note_events = np.c_[note_events[:-1], np.diff(note_events[:, 1])]
 
         # loop over bars
-        bar_grid = np.linspace(0, 1, self.pr_bar_division)
+        beat_grid = np.linspace(0, 1, self.pr_bar_division)
 
         for cur_bar in range(int(note_events[0][1]), int(note_events[-1][1])):
             # get the notes which belong to the bar
@@ -63,18 +63,26 @@ class ImporterRollingStone(ImporterBase):
 
             # what are the nearest notes
             offset = int(cur_notes[0][0])
-
+            prev_note_idx_end = -1
             for cur_note in cur_notes:
-                note_idx_start = np.argmin(cur_note[1]-bar_grid)
-                duration = int(1.0 / cur_note[4])
+                metric_timing = cur_note[1] - int(cur_note[1])
+                #find the closest beat on the beat_grid
+                note_idx_start = np.argmin(abs(metric_timing-beat_grid))
+                if prev_note_idx_end > note_idx_start:
+                    print('bad!')
+
+                note_start_diff = (metric_timing - beat_grid)[note_idx_start]
+                duration = int(cur_note[4] / (1.0 / self.pr_bar_division))
                 note_idx_end = note_idx_start + duration
                 cur_pitch = cur_note[2]
+                prev_note_idx_end = note_idx_end
+
 
                 # add to piano-roll
                 cur_bar_idx_start = cur_bar*self.pr_bar_division
                 cur_bar_idx_end = (cur_bar+1)*self.pr_bar_division
                 piano_roll[cur_pitch, cur_bar_idx_start+note_idx_start:cur_bar_idx_start+note_idx_end] = 1
-
+        prev_note_idx_end = -1
         import matplotlib.pyplot as plt
         plt.imshow(piano_roll, cmap=plt.get_cmap('gray_r'))
         plt.show()
