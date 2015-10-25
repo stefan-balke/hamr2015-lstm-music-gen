82d61cddb0d58d84fd3f2a99a1f5ed7b717d0178
Ubuntu <ubuntu@ip-172-31-0-85.us-west-1.compute.internal>
2015-10-25 15:10:14 +0000

    ilimitied essen to 200, changed topology # nodes

--- compose.py
+++ compose.py
@@ -52,9 +52,9 @@ class Composer:
 
         print 'num_features =', num_features
         self.model = Sequential()
-        self.model.add(LSTM(50, return_sequences=True, input_shape=(self.window_size, num_features)))
+        self.model.add(LSTM(36, return_sequences=True, input_shape=(self.window_size, num_features)))
         self.model.add(Dropout(0.2))
-        self.model.add(LSTM(50, return_sequences=False))
+        self.model.add(LSTM(36, return_sequences=False))
         self.model.add(Dropout(0.2))
         self.model.add(Dense(num_features))
         #self.model.add(Activation('relu'))  # Rectified Linear Unit # TODO: 'relu' missing in my theano install
@@ -68,7 +68,7 @@ class Composer:
         """
 
         # Chop up all songs in dataset into examples with window-size N
-        training_examples = self._get_training_examples()  # [0:20]
+        training_examples = self._get_training_examples()[0:200]
         #print 'training_examples:', training_examples
         print '# training sequences:', len(training_examples)
 
@@ -99,7 +99,7 @@ class Composer:
             print '-' * 50
             print 'Iteration', iteration
 
-            self.model.fit(training_examples_X, training_examples_y, batch_size=128, nb_epoch=n_epoch)  # TODO: batch_size = 128
+            self.model.fit(training_examples_X, training_examples_y, batch_size=64, nb_epoch=n_epoch)  # TODO: batch_size = 128
 
             self.compose(iteration)
 
@@ -108,9 +108,13 @@ class Composer:
         """
         np.set_printoptions(threshold=np.nan)
 
-        SEED = self.dataset[3].transpose()
-        melody = SEED[:self.window_size-1]  # Use the window at the end. Subtract 1 since normal window size includes prediction.
-        melody = np.expand_dims(melody, axis=0)
+        SEED = self.dataset[0].transpose()
+
+        #SEED = np.random.randint(0, 2, size=(64, FEATURE_VECTOR_LENGTH))
+
+
+        SEED = SEED[:self.window_size-1]  # Use the window at the end. Subtract 1 since normal window size includes prediction.
+        melody = np.expand_dims(SEED, axis=0)
         print "melody shape:", melody.shape
         print "Seed length:", len(SEED)
       
@@ -118,11 +122,11 @@ class Composer:
         print '----- Generating with seed:', melody
 
         for i in range(num_measures * BEATS_PER_MEASURE - len(SEED)):
-            print 'melody.shape', melody.shape
+            #print 'melody.shape', melody.shape
             x = np.expand_dims(np.array(melody[0][i:i + self.window_size]), axis=0)
-            print 'i:', i
-            print 'x:', x
-            print 'x.shape', x.shape
+            #print 'i:', i
+            #print 'x:', x
+            #print 'x.shape', x.shape
 
             next_frame = self.model.predict(x, verbose=0)[0]
 
@@ -133,15 +137,13 @@ class Composer:
             next_frame = self._get_binary_vector(next_frame)
             next_frame = np.expand_dims(next_frame, axis=0)
 
-            print 'next_frame normalized:', next_frame
-            print 'melody.shape', melody.shape
-            print 'next_frame.shape', next_frame.shape
+            #print 'next_frame normalized:', next_frame
+            #print 'melody.shape', melody.shape
+            #print 'next_frame.shape', next_frame.shape
 
 
             melody = np.concatenate([melody, np.expand_dims(next_frame, axis=0)], axis=1)
-            print 'Appended melody:', melody
-
-            print 'end of for'
+            #print 'Appended melody:', melody
 
         print 'Final melody:', melody[0]
         print
@@ -181,9 +183,10 @@ class Composer:
 
 
 if __name__ == '__main__':
-    data_rs = ImporterRollingStone(BEATS_PER_MEASURE, MELODY_INDICES_RANGE, HARMONY_INDICES_RANGE, CONTINUATION_FLAG_RANGE, METRIC_FLAGS_RANGE)
-    #data_essen = ImporterEssen()
+    #data_rs = ImporterRollingStone(BEATS_PER_MEASURE, MELODY_INDICES_RANGE, HARMONY_INDICES_RANGE, CONTINUATION_FLAG_RANGE, METRIC_FLAGS_RANGE)
+    data_essen = ImporterEssen()
 
-    bach = Composer(data_rs.output)
+    #bach = Composer(data_rs.output)
+    bach = Composer(data_essen.output)
     bach.train()
 
