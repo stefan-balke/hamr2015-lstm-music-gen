b0167263327672ce54f1a27e480f17a78824fc4d
Ubuntu <ubuntu@ip-172-31-0-85.us-west-1.compute.internal>
2015-10-26 09:54:26 +0000

    Server changes before long trianing run.

--- compose.py
+++ compose.py
@@ -9,6 +9,7 @@ import sys
 
 from importer.rolling_stone import ImporterRollingStone
 from importer.essen import ImporterEssen
+from importer.essen_untransposed import EssenUntransposed
 from exporter.neural_net_to_midi import MidiExporter
 
 from settings import *
@@ -117,65 +118,56 @@ class Composer:
         """Use a pre-trained neural network to compose a melody.
         """
         np.set_printoptions(threshold=np.nan)
-
         for diversity in [0.2, 0.5, 1.0, 1.2]:
-            print
+	    print
             print '----- diversity:', diversity
 
-        for i in range(num_measures * BEATS_PER_MEASURE - len(SEED)):
-            #print 'melody.shape', melody.shape
-            x = np.expand_dims(np.array(melody[0][i:i + self.window_size]), axis=0)
-            #print 'i:', i
-            #print 'x:', x
-            #print 'x.shape', x.shape
+	    SEED = self.dataset[3].transpose()
+	    SEED = SEED[self.window_size-1:]  # Use the window at the start. Subtract 1 since normal window size includes prediction.
+	    melody = np.expand_dims(SEED, axis=0)
+	    print len(SEED)
+	    print len(melody)
 
-            SEED = self.dataset[3].transpose()
-            #SEED = SEED[:self.window_size-1]  # Use the window at the end. Subtract 1 since normal window size includes prediction.
-            SEED = SEED[self.window_size-1:]  # Use the window at the start. Subtract 1 since normal window size includes prediction.
-            melody = np.expand_dims(SEED, axis=0)
-            print len(SEED)
-            print len(melody)
+	    print '----- Generating with seed:', melody
 
-            print '----- Generating with seed:', melody
+	    for i in range(num_measures * BEATS_PER_MEASURE - len(SEED)):
+		print 'melody.shape', melody.shape
+		x = np.expand_dims(np.array(melody[0][i:i + self.window_size]), axis=0)
+		print 'i:', i
+		print 'x:', x
+		print 'x.shape', x.shape
 
-            for i in range(num_measures * BEATS_PER_MEASURE - len(SEED)):
-                print 'melody.shape', melody.shape
-                x = np.expand_dims(np.array(melody[0][i:i + self.window_size]), axis=0)
-                print 'i:', i
-                print 'x:', x
-                print 'x.shape', x.shape
+		next_frame = self.model.predict(x, verbose=0)[0]
 
-                next_frame = self.model.predict(x, verbose=0)[0]
+		#print 'next_frame normalized:', next_frame
+		#print 'melody.shape', melody.shape
+		#print 'next_frame.shape', next_frame.shape
+		print 'next_frame raw:', next_frame
 
-                #print 'next_frame normalized:', next_frame
-                #print 'melody.shape', melody.shape
-                #print 'next_frame.shape', next_frame.shape
-                print 'next_frame raw:', next_frame
+		if SAMPLE_FROM_MELODY_PROBS:
+		    # sample from melody probabilities.
+		    next_frame = self._sample_melody(next_frame, MELODY_INDICES_RANGE, diversity)
+		else:
+		    # Winner-takes-all on melody to force monophonic, and force other floats in vector to 0 or 1.
+		    next_frame = self._winner_takes_all(next_frame, MELODY_INDICES_RANGE)
 
-                if SAMPLE_FROM_MELODY_PROBS:
-                    # sample from melody probabilities.
-                    next_frame = self._sample_melody(next_frame, MELODY_INDICES_RANGE, diversity)
-                else:
-                    # Winner-takes-all on melody to force monophonic, and force other floats in vector to 0 or 1.
-                    next_frame = self._winner_takes_all(next_frame, MELODY_INDICES_RANGE)
+		next_frame = self._get_binary_vector(next_frame)
+		next_frame = np.expand_dims(next_frame, axis=0)
 
-                next_frame = self._get_binary_vector(next_frame)
-                next_frame = np.expand_dims(next_frame, axis=0)
-
-                print 'next_frame normalized:', next_frame
-                print 'melody.shape', melody.shape
-                print 'next_frame.shape', next_frame.shape
+		print 'next_frame normalized:', next_frame
+		print 'melody.shape', melody.shape
+		print 'next_frame.shape', next_frame.shape
 
 
-                melody = np.concatenate([melody, np.expand_dims(next_frame, axis=0)], axis=1)
-                print 'Appended melody:', melody
+		melody = np.concatenate([melody, np.expand_dims(next_frame, axis=0)], axis=1)
+		print 'Appended melody:', melody
 
-                print 'end of for'
+		print 'end of for'
 
-            print 'Final melody:', melody
-            print
-            exporter = MidiExporter(melody[0])
-            exporter.create_midi_file('random_%d_%.2f.midi' % (index, diversity))
+	    print 'Final melody:', melody
+	    print
+	    exporter = MidiExporter(melody[0])
+	    exporter.create_midi_file('random_%d_%.2f.midi' % (index, diversity))
 
     def _get_training_examples(self):
         """Return N - window_size example matrices, each with window_size vectors.
@@ -221,7 +213,7 @@ class Composer:
 
 if __name__ == '__main__':
     #data_rs = ImporterRollingStone(BEATS_PER_MEASURE, MELODY_INDICES_RANGE, HARMONY_INDICES_RANGE, CONTINUATION_FLAG_RANGE, METRIC_FLAGS_RANGE)
-    data_essen = ImporterEssen()
+    data_essen = EssenUntransposed()
 
     #bach = Composer(data_rs.output)
     bach = Composer(data_essen.output)
